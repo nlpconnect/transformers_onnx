6697dd9b70d4ae802e51fc3dead63191ec310861
Ankur3107 <ankur310794@gmail.com>
2022-04-18 17:15:24 +0530

    folder name changed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     name='transformer_onnx',  
+     name='transformers_onnx',  
      version='0.0.1',
      author="NLP Connect",
      author_email="",
